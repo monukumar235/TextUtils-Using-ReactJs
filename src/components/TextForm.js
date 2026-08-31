d168e78bd701681a941f038f2d67aa751ff418da
Monu Kumar <monuagarwal235@gmail.com>
2026-08-31 16:35:28 +0530

    fixed bugs

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,71 +3,81 @@ import PropTypes from "prop-types";
 
 export default function TextForm(props) {
   const [text, setText] = useState("");
-  
 
   const handleUpperCaseClick = () => {
     let newText = text.toUpperCase();
     setText(newText);
-    props.showAlert("Converted to UpperCase!","success");
+    props.showAlert("Converted to UpperCase!", "success");
   };
-  
+
   const handleLowerCaseClick = () => {
     let newtext = text.toLowerCase();
     setText(newtext);
-    props.showAlert("Converted to LowerCase!","success");
+    props.showAlert("Converted to LowerCase!", "success");
   };
-  
+
   const handleOnChange = (event) => {
     setText(event.target.value);
   };
-  
+
   const handleClearClick = () => {
     let newtext = "";
     setText(newtext);
-    props.showAlert("Cleared!","success");
+    props.showAlert("Cleared!", "success");
   };
-  
+
   const handleCapitalizedClick = () => {
     let words = text.split(" ");
-    
+
     for (let i = 0; i < words.length; i++) {
       words[i] =
-      words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
+        words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
     }
     setText(words.join(" "));
-    props.showAlert("Converted to Capitalized Case!","success");
+    props.showAlert("Converted to Capitalized Case!", "success");
   };
-  
+
   const handleInvertCaseClick = () => {
     let words = text.split(" ");
-    
+
     for (let i = 0; i < words.length; i++) {
       words[i] =
-      words[i].charAt(0).toLowerCase() + words[i].slice(1).toUpperCase();
+        words[i].charAt(0).toLowerCase() + words[i].slice(1).toUpperCase();
     }
     setText(words.join(" "));
-    props.showAlert("Converted to Inverse Capitalized Case!","success");
+    props.showAlert("Converted to Inverse Capitalized Case!", "success");
   };
-  
+
   const copyText = () => {
     let text = document.getElementById("myBox");
     text.select();
     navigator.clipboard.writeText(text.value);
     handleClearClick();
-    props.showAlert("Text has been copied to clipboard!","success");
+    props.showAlert("Text has been copied to clipboard!", "success");
   };
-  
+
   const reomveExtraSpaces = () => {
     let newText = text.split(/[  ]+/);
     setText(newText.join(" "));
-    props.showAlert("Removed the extra space from the text!","success");
+    props.showAlert("Removed the extra space from the text!", "success");
   };
 
-  
-
+  const countWords = ()=>{
+    let wordArr = text.split(" ");
+    let count =0;
+    for(let i=0;i<wordArr.length;i++){
+      if(wordArr[i]!==""){
+        count++;
+      }
+    }
+    return count;
+  }
   return (
     <>
-      <div className="container"  style={{color : props.mode === 'dark' ? 'white' : 'black'}}>
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "#343a40" }}
+      >
         <div className="container">
           <h1>{props.heading}</h1>
           <div className="mb-3">
@@ -77,53 +87,65 @@ export default function TextForm(props) {
               id="myBox"
               rows="8"
               onChange={handleOnChange}
-              style={{backgroundColor : props.mode === 'dark' ? 'grey' : 'white',color: props.mode ==='dark' ? 'white' : 'black'}}
-            ></textarea>
+              style={{
+                backgroundColor: props.mode === "dark" ? "#343a40" : "white",
+                color: props.mode === "dark" ? "white" : "grey"
+              }}
+            >
+            </textarea>
           </div>
 
           <button
-            className="btn btn-primary mx-1"
+            className="btn mx-1"
             onClick={handleUpperCaseClick}
+            style={{backgroundColor: props.mode === 'dark' ? 'white' : '#343a40',color :props.mode === 'dark'?'black':'white'}}
           >
             Convert to Uppercase
           </button>
           <button
-            className="btn btn-primary mx-1"
+            className="btn mx-1"
+            style={{backgroundColor: props.mode === 'dark' ? 'white' : '#343a40',color :props.mode === 'dark'?'black':'white'}}
             onClick={handleLowerCaseClick}
           >
             Convert to Lowercase
           </button>
-          <button className="btn btn-primary mx-1" onClick={handleClearClick}>
+          <button className="btn mx-1" style={{backgroundColor: props.mode === 'dark' ? 'white' : '#343a40',color :props.mode === 'dark'?'black':'white'}} onClick={handleClearClick}>
             Clear Text
           </button>
           <button
-            className="btn btn-primary mx-1"
+            className="btn mx-1"
+            style={{backgroundColor: props.mode === 'dark' ? 'white' : '#343a40',color :props.mode === 'dark'?'black':'white'}}
             onClick={handleCapitalizedClick}
           >
             Capitalized Text
           </button>
           <button
-            className="btn btn-primary mx-1"
+            className="btn mx-1"
             onClick={handleInvertCaseClick}
+            style={{backgroundColor: props.mode === 'dark' ? 'white' : '#343a40',color :props.mode === 'dark'?'black':'white'}}
           >
             Invert Case Text
           </button>
-          <button className="btn btn-primary mx-1" onClick={copyText}>
+          <button className="btn mx-1" onClick={copyText} style={{backgroundColor: props.mode === 'dark' ? 'white' : '#343a40',color :props.mode === 'dark'?'black':'white'}}>
             Copy Text
           </button>
-          <button className="btn btn-primary mx-1" onClick={reomveExtraSpaces}>
+          <button className="btn  mx-1" onClick={reomveExtraSpaces} style={{backgroundColor: props.mode === 'dark' ? 'white' : '#343a40',color :props.mode === 'dark'?'black':'white'}}>
             Remove Extra Spaces
           </button>
         </div>
         <div className="container my-3">
           <h2>Your text summary</h2>
           <p>
-            {text.split(" ").length} word and {text.length} characters
+            {countWords()} word and {text.length} characters
           </p>
           <p>{0.008 * text.split(" ").length} minutes to read</p>
 
           <h2>Preview</h2>
-          <p>{text.length>0 ? text : "Enter something above in textbox to preview here...."}</p>
+          <p>
+            {text.length > 0
+              ? text
+              : "Enter something above in textbox to preview here...."}
+          </p>
         </div>
       </div>
     </>
