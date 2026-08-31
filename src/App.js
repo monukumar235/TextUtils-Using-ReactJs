51bd72b19c692b1407777be792a1768278961121
Monu Kumar <monuagarwal235@gmail.com>
2026-08-31 08:56:44 +0530

    added alert feature here

--- src/App.js
+++ src/App.js
@@ -2,18 +2,34 @@ import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
 // import About from "./components/About";
 
 function App() {
   const [mode, setMode] = useState("light");
 
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message,type)=>{
+    setAlert({
+      msg : message,
+      type : type
+    });
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
   const toggleMode = () => {
     if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "grey";
+      showAlert("Dark mode has been enabled","success");
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled","success");
     }
   };
   return (
@@ -24,9 +40,10 @@ function App() {
         mode={mode}
         toggleMode={toggleMode}
       />
+      <Alert alert = {alert}/>
       <div className="container my-3">
         {/* <About/> */}
-        <TextForm heading="Enter the text to analyze" mode = {mode} />
+        <TextForm heading="Enter the text to analyze" mode = {mode} showAlert ={showAlert}/>
       </div>
     </>
   );
